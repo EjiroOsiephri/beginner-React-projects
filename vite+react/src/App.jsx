bfdd8d0d8eefd398214569f63c6381501e6e22a0
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-03-05 22:04:42 +0100

    Understood props

--- vite+react/src/App.jsx
+++ vite+react/src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 import Texts from "./components/texts";
 import Card from "./components/Card";
+import "./components/card.css";
 
 const App = () => {
   return (
@@ -9,7 +10,26 @@ const App = () => {
       <NavBar />
       <Main />
       <Texts />
-      <Card />
+      <div className="myDiv">
+        <Card
+          name="Ejiro"
+          img="../components/image.png"
+          number="08067564536"
+          class="ss3"
+        />
+        <Card
+          name="Mike"
+          img="../components/image.png"
+          number="08067564536"
+          class="ss3"
+        />
+        <Card
+          name="Dave"
+          img="../components/image.png"
+          number="08067674536"
+          class="s23"
+        />
+      </div>
     </div>
   );
 };
--- vite+react/src/components/Card.jsx
+++ vite+react/src/components/Card.jsx
@@ -1,18 +1,22 @@
 import "../components/card.css";
-import IMG from "../components/image 12.png";
+// import IMG from "../components/image 12.png";
 import Star from "../components/Star 1.png";
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className="card">
-      <img className="card-img" src={IMG} alt="" />
-      <div className="flex-container">
-        <img src={Star} alt="" />
-        <small>5.0</small>
-        <small>(6)-USA</small>
+    <div className="all">
+      <div className="card">
+        <img className="card-img" src={props.img} alt="" />
+        <div className="flex-container">
+          <h4>{props.name}</h4>
+          <img src={Star} alt="" />
+          <small>5.0</small>
+          <small>(6)-USA</small>
+          <p>{props.number}</p>
+        </div>
+        <h5>Life lessons with Katie Zaferes</h5>
+        <h5>From $136 / Person</h5>
       </div>
-      <h5>Life lessons with Katie Zaferes</h5>
-      <h5>From $136 / Person</h5>
     </div>
   );
 };
