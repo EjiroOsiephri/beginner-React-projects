9dc476543c14680ac1d36c1f85cdcfa4c8b2b2f7
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-03-06 21:29:25 +0100

    Air Bnb clone finished

--- vite+react/src/App.jsx
+++ vite+react/src/App.jsx
@@ -3,31 +3,43 @@ import Main from "./components/Main";
 import Texts from "./components/texts";
 import Card from "./components/Card";
 import "./components/card.css";
+import Data from "./components/data";
 
 const App = () => {
+  const newData = Data.map((dataValue) => {
+    return dataValue;
+  });
+  console.log(newData);
   return (
     <div>
       <NavBar />
       <Main />
       <Texts />
+
       <div className="myDiv">
         <Card
-          name="Ejiro"
-          img="../components/image.png"
-          number="08067564536"
-          class="ss3"
+          name={newData[0].name}
+          title={newData[0].title}
+          img={newData[0].coverImg}
+          number={newData[0].description}
+          location={newData[0].location}
+          openSpots={newData[0].openSpots}
         />
         <Card
-          name="Mike"
-          img="../components/image.png"
-          number="08067564536"
-          class="ss3"
+          name={newData[1].name}
+          title={newData[1].title}
+          img={newData[1].coverImg}
+          number={newData[1].description}
+          location={newData[1].location}
+          openSpots={newData[1].openSpots}
         />
         <Card
-          name="Dave"
-          img="../components/image.png"
-          number="08067674536"
-          class="s23"
+          name={newData[2].name}
+          title={newData[2].title}
+          img={newData[2].coverImg}
+          number={newData[2].description}
+          location={newData[2].location}
+          openSpots={newData[2].openSpots}
         />
       </div>
     </div>
--- vite+react/src/components/Card.jsx
+++ vite+react/src/components/Card.jsx
@@ -3,19 +3,30 @@ import "../components/card.css";
 import Star from "../components/Star 1.png";
 
 const Card = (props) => {
+  // const openSpots = 0;
+  // const trueValue = openSpots ? (display = "block") : (display = "none");
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  } else {
+  }
   return (
     <div className="all">
       <div className="card">
-        <img className="card-img" src={props.img} alt="" />
+        {badgeText && <div className="card--badge">{badgeText}</div>}
+        <img className="card-img" src={`/${props.img}`} alt="" />
         <div className="flex-container">
           <h4>{props.name}</h4>
+          <h4>{props.title}</h4>
           <img src={Star} alt="" />
           <small>5.0</small>
-          <small>(6)-USA</small>
+          <small>
+            (6)-<span>{props.location}</span>
+          </small>
           <p>{props.number}</p>
         </div>
-        <h5>Life lessons with Katie Zaferes</h5>
-        <h5>From $136 / Person</h5>
       </div>
     </div>
   );
